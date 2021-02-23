503920fbf60da2b12ebff6a24a29d8608e6220c9
chad-bennett <chad.bennett@live.com>
2021-02-23 16:02:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# demo site
+ a template for making websites
---
+++ deprecated/nav2.js
@@ -0,0 +1,12 @@
+document.write('<li><a href="#">Home</a></li>')
+document.write('<li class="dropdown">')
+document.write('<button type="button" class="dropdown__title" aria-expanded="false" aria-controls="sweets-dropdown">Dropdown</button>')
+document.write('<ul class="dropdown__menu" id="sweets-dropdown">')
+document.write('<li><a href="#">this</a></li>')
+document.write('<li><a href="#">that</a></li>')
+document.write('<li><a href="#">other</a></li>')
+document.write('</ul>')
+document.write('</li>')
+document.write('<li><a href="#">Regular</a></li>')
+document.write('<li><a href="#">Stuff</a></li>')
+
